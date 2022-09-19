8ffbf2a448cd5f59888fdceb4fae01d2e13c57e6
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-19 16:37:46 +1000

    commit tree 5fb81502a845b978ec220e7707ad0597759c52a6

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+//////// DEMONSTRATION /////////
+//reduce array method takes in two arguments: the callback function and optional start value assigned to the accumulator
+//the final value of the accumulator is returned
+//In the absence of an initial value, the first element in the array is used as the starting value. Watch out lavee e_e
+const doubledAndSummed = [1, 2, 3].reduce(function(accumulator, element){return element * 2 + accumulator}, 0);
+console.log(doubledAndSummed);
+
+const letters = ['a', 'b', 'c', 'b', 'a', 'a'];
+const lettersCount = letters.reduce(function(countObj, currentLetter){
+    if (currentLetter in countObj) {
+        countObj[currentLetter]++;
+    } else {
+        countObj[currentLetter] = 1;
+    }
+    return countObj;
+}, {});
+console.log(lettersCount);
+
+/////// LAB ////////
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const totalBatteries = batteryBatches.reduce((accumulator, element) => accumulator + element, 0);
+console.log(totalBatteries);
